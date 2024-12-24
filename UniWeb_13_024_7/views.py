5561f1d718cd10fd02779014446bbeea05116090
IBraHkar <malisciencetec2.0@gmail.com>
2024-12-24 15:24:44 +0000

    Correction du 24/12/2024 15:24

--- UniWeb_13_024_7/views.py
+++ UniWeb_13_024_7/views.py
@@ -9,16 +9,12 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.core.paginator import Paginator
 from django.http import JsonResponse
-from django.views.decorators.http import require_POST
-from django.views.decorators.csrf import csrf_protect
 # Create your views here.
 import os
 
 
 
 @login_required
-@csrf_protect
-@require_POST
 def like_document(request,pdf_name,user_id,etat):
     if request.method == 'POST':
         user=get_object_or_404(User,pk=user_id)
@@ -67,8 +63,6 @@ def like_document(request,pdf_name,user_id,etat):
 
 
 @login_required
-@csrf_protect
-@require_POST
 def like_event(request,event_id,user_id,etat):
     if request.method=='POST':
         try:
@@ -99,8 +93,6 @@ def like_event(request,event_id,user_id,etat):
             return JsonResponse({})
 
 @login_required
-@csrf_protect
-@require_POST
 def FavorisGestion(request,pdf_name,user_id,etat):
     if request.method == 'POST':
         user= get_object_or_404(User,pk=user_id)
